221bc5d80a03278653ef706bd6a902d59ad4a892
junior-pacheco <elkinjuniorpa09@gmail.com>
2022-09-27 15:47:10 -0500

    Device listo,input listo,lan listo,output listo, wiegand listo,inicio listo

--- Pagina/pagina.js
+++ Pagina/pagina.js
@@ -1,16 +1,16 @@
-// document.getElementById("myButton").addEventListener("click", ()=>{
-//     window.location.href = "/index.html"
+document.getElementById("mybutton").addEventListener("click", ()=>{
+    window.location.href = "./index.html"
 
-// });
+});
 
 
 
-const urls = ["index1.html","index2.html"]
+// const urls = ["index1.html","index2.html"]
 
-const buttons = document.querySelectorAll(".btn")
+// const buttons = document.querySelectorAll(".btn")
 
-buttons.forEach((elemt, i) => {
-    elemt.addEventListener('click', () => {
-        window.location.href = urls[i]
-    })
-})
+// buttons.forEach((elemt, i) => {
+//     elemt.addEventListener('click', () => {
+//         window.location.href = urls[i]
+//     })
+// })
